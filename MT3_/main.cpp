9fdc23fdcf5c4fdecac6970ea1cc4aebb95badfb
tsubasak1217 <k023g0112@g.neec.ac.jp>
2024-04-13 17:50:36 +0900

    MT3_00_02

--- MT3_/main.cpp
+++ MT3_/main.cpp
@@ -13,6 +13,24 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	char keys[256] = { 0 };
 	char preKeys[256] = { 0 };
 
+	Matrix4x4 m1(
+		3.2f, 0.7f, 9.6f, 4.4f,
+		5.5f, 1.3f, 7.8f, 2.1f,
+		6.9f, 8.0f, 2.6f, 1.0f,
+		0.5f, 7.2f, 5.1f, 3.3f
+	);
+
+	Matrix4x4 m2(
+		4.1f, 6.5f, 3.3f, 2.2f,
+		8.8f, 0.6f, 9.9f, 7.7f,
+		1.1f, 5.5f, 6.6f, 0.0f,
+		3.3f, 9.9f, 8.8f, 2.2f
+	);
+
+	int kRowHeight = 120;
+	int kColWidth = 280;
+
+
 	// ウィンドウの×ボタンが押されるまでループ
 	while (Novice::ProcessMessage() == 0) {
 		// フレームの開始
